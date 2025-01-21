9c3d1eb29004e0204a5a31e5809f0151ee2d53f7
Muhammad Al-fatih Ritonga <muhammadalfatihritonga22@gmail.com>
2025-01-21 21:41:58 +0700

    membuat handler dengan httprouter

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Hello HttpRouter")
+	})
+
+	server := http.Server{
+		Handler: router,
+		Addr:    "localhost:3000",
+	}
+
+	fmt.Println("Server berjalan di http://localhost:3000")
+	server.ListenAndServe()
+}
