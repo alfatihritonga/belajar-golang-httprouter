568d689230475b76e6a8e4f404cfab446f7ed96f
Muhammad Al-fatih Ritonga <muhammadalfatihritonga22@gmail.com>
2025-01-22 22:24:19 +0700

    Middleware

---
+++ middleware_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+type LogMiddleware struct {
+	Handler http.Handler
+}
+
+func (middleware *LogMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Middleware")
+	middleware.Handler.ServeHTTP(w, r)
+}
+
+func Test_Middleware(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	middleware := LogMiddleware{router}
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	middleware.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello World", string(body))
+}
