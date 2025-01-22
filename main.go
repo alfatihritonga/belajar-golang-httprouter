2df6db0de565776c9fba6866ea82a99bb7c0eddd
Muhammad Al-fatih Ritonga <muhammadalfatihritonga22@gmail.com>
2025-01-22 21:08:34 +0700

    Not Found Handler

--- main.go
+++ main.go
@@ -9,11 +9,20 @@ import (
 
 func main() {
 	router := httprouter.New()
+
 	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, i interface{}) {
 		fmt.Fprint(w, "Error : ", i)
 	}
+
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Oops! The page you are looking for was not found")
+	})
+
 	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		// fmt.Fprint(w, "Hello HttpRouter")
+		fmt.Fprint(w, "Hello HttpRouter")
+	})
+
+	router.GET("/panic", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		panic("internal server error")
 	})
 
